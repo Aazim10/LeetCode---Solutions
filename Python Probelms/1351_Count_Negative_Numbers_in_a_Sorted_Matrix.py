1400e0b396c3c4e2a9dc84f223d6d82b7f2e80aa
Aazim Ahmed <108620737+Aazim10@users.noreply.github.com>
2023-10-07 16:02:07 +0500

    Update 1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py

--- Python Probelms/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
+++ Python Probelms/1351_Count_Negative_Numbers_in_a_Sorted_Matrix.py
@@ -1,3 +1,7 @@
+# I have two solutions for this.
+# NO.1 has better Space Complexity
+# NO.2 has better Time Complexity
+# Solution 1
 class Solution(object):
     def countNegatives(self,grid):
         """
@@ -22,6 +26,38 @@ class Solution(object):
                     lo = mid + 1
         return neg_num
 
+# Solution 2
+class Solution(object):
+    def countNegatives(self,grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        neg_num = 0
+        grid_rlen = len(grid)
+        grid_clen = len(grid[0])
+        for i in range(grid_rlen):
+            lo = 0
+            hi = len(grid[0])-1
+            while lo<=hi:
+                mid = (lo+hi)//2
+                mid_val =  grid[i][mid]
+                if mid_val < 0:
+                    if (mid-1)>=0 and grid[i][mid-1]<0:
+                        hi = mid - 1
+                    else:
+                        neg_num = neg_num + (grid_clen-mid)
+                        lo = hi+1
+                elif mid_val >= 0:
+                    lo = mid + 1
+        return neg_num
+
+
+
+
+
+
+        
 
 
 
